c082d607075f798595d40489efa821247662583c
yuchangjae <yugambiyugambi@gmail.com>
2026-01-04 18:17:17 +0900

    first commit

---
+++ 2.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//
+//int main() {
+//    int number;
+//
+//    printf("정수를 입력해주세요 : ");
+//    scanf_s("%d", &number);
+//
+//    if (number > 0) {
+//        printf("입력받은 숫자는 양수입니다!");
+//    }
+//    else if (number == 0) {
+//        printf("양수도 음수도 아닌 0 입니다!");
+//    }
+//    else {
+//        printf("입력받은 숫자는 음수입니다!");
+//    }
+//
+//    return 0;
+//}
