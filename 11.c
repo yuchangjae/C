103506656492d61f4657a2854bbff85981b471d5
yuchangjae <yugambiyugambi@gmail.com>
2026-01-06 22:07:47 +0900

    Feat: add multiplication table and star patterns using nested loops

---
+++ 11.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int i, j;
+//	int a;
+//	scanf("%d", &a);
+//
+//	for (i = 1; i <= a; i++) {
+//		for (j = 1; j <= a; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//
+//	return 0;
+//}
---
+++ 12.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int i, j, k;
+	int a;
+	int aa = 1;
+	scanf("%d", &a);
+
+	for(i = a; i >= 1; i--){
+		for (j = 1; j <= i; j++){
+			printf(" ");
+		}
+
+		for (int p = 0; p < aa; p++) {
+			printf("*");
+		}
+
+		aa += 2;
+		printf("\n");
+	}
+	return 0;
+}
