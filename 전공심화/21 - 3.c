e635969ec9033454539f053a408822c5a8d0d185
yuchangjae <yugambiyugambi@gmail.com>
2026-02-14 22:36:05 +0900

    Refactor: review and refine nested loop exercises

--- 전공심화/21 - 3.c
+++ 전공심화/21 - 3.c
@@ -1,15 +1,15 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main() {
-	int a;
-	int i;
-	int j;
-	scanf("%d", &a);
-	for (i = a; i > 0; i--) {
-		for (j = 1; j <= i; j++) {
-			printf("*");
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main() {
+//	int a;
+//	int i;
+//	int j;
+//	scanf("%d", &a);
+//	for (i = a; i > 0; i--) {
+//		for (j = 1; j <= i; j++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
